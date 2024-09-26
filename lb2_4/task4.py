d8e3ab0620835800dc455db63e6f6f121019d64d
Seregun <seregunmgp@gmail.com>
2024-09-26 10:55:56 +0300

    Update task4.py

--- lb2_4/task4.py
+++ lb2_4/task4.py
@@ -1,5 +1,5 @@
-import math;
-x = float(input("Введите х: "));
-y = float(input("Введите y: "));
-u=(((8+abs(x-y)**2+1)**(1/3))/(x**2+y**2+2))-math.e**(abs(x-y));
-print("u = ", u);
+import math
+x = float(input("Введите х: "))
+y = float(input("Введите y: "))
+u=(((8+abs(x-y)**2+1)**(1/3))/(x**2+y**2+2))-math.e**(abs(x-y))
+print("u = ", u)
